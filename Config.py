b7925532bc058d6f47df6d0b7a1f908fbb90237c
ksbotoffical2 <github671@outlook.com>
2025-05-13 11:48:25 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -10,7 +10,7 @@ API_HASH =  "914be3dde138ba7e513eebaf88a83c14"
 # For /log cmd
 OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "6344607551").split(" ")]
 # No time limit for this users
-AUTH_USERS = [6344607551]
+AUTH_USERS = [6344607551, 6186862886]
 # Time gap after each request (in seconds) 
 TIME_GAP = 0
 BOT_USERNAME='testiverecordingbot' #bot username without@
