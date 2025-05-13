62460ccc5fb9891c37db067bdf542c5e789f7d21
ksbotoffical2 <github671@outlook.com>
2025-05-13 11:30:25 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,16 +3,16 @@
 import os
 
 # Bot token from @botfather
-BOT_TOKEN = ""
+BOT_TOKEN = "7699051363:AAFnmcRhUs15EQFpKOfj_1wewTN-IwTS2SI"
 # From my.telegram.org/
-API_ID = ""
-API_HASH =  ""
+API_ID = "7880996"
+API_HASH =  "914be3dde138ba7e513eebaf88a83c14"
 # For /log cmd
-OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "969084369").split(" ")]
+OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "6344607551").split(" ")]
 # No time limit for this users
-AUTH_USERS = [969084369, 2800583882]
+AUTH_USERS = [6344607551]
 # Time gap after each request (in seconds) 
 TIME_GAP = 0
-BOT_USERNAME='test' #bot username without@
+BOT_USERNAME='testiverecordingbot' #bot username without@
 API_TOKEN='b7d9e1f7-12a6-4b8e-89a2-c3d124e5f6a7' #don't change this without developer permission
 
